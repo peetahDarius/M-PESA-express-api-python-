1288e2cdae6b18f6da33fe516ef1bc32d89ffdf6
Darius Peter Kariuki <76100534+peetahDarius@users.noreply.github.com>
2023-08-09 07:53:02 +0300

    Update mpesa_express_api.py

--- mpesa_express_api.py
+++ mpesa_express_api.py
@@ -23,10 +23,22 @@ json_data = {
         The below consumer key and consumer secret will not work since by the time you are using this code, they will have expired.
 """
 
-
+# if using the code in the mpesa express sandbox
 def stk_iniitiate():
     consumer_key = 'GVdkmTSajtHRd3665YdQrvbbBisNAGz0ubC'
     consumer_secret = 'VlK6efsGw1Hg745V'
+    mpesaobj = MpesaExpress(consumer_key=consumer_key, consumer_secret=consumer_secret)
+    response = mpesaobj.initiate_transaction(json_data=json_data)
+    return response
+
+""" Feel freee to coment out any of the two stk initiate functions. use the above for sandbox testing and the below one for going live[production] """
+
+# if using the code for production ( we must change the mpesa endpoints)
+def stk_iniitiate():
+    consumer_key = 'GVdkmTSajtHRd3665YdQrvbbBisNAGz0ubC'
+    consumer_secret = 'VlK6efsGw1Hg745V'
+    MpesaExpress.production_urls(authorization_url='https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+                             stk_initiate_url='https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest')
     mpesaobj = MpesaExpress(consumer_key=consumer_key, consumer_secret=consumer_secret)
     response = mpesaobj.initiate_transaction(json_data=json_data)
     return response
